f1b82effcac2126d6dd4ec4ec4007ee0af47aa96
Deon Curry <deoncvn68@gmail.com>
2025-03-19 17:40:48 -0400

    nav bar wip

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,31 +1,42 @@
 import { useContext } from 'react';
-import { Link } from 'react-router';
-
+import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../../contexts/UserContext';
+import './NavBar.css';
 
 const NavBar = () => {
   const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
   const handleSignOut = () => {
     localStorage.removeItem('token');
     setUser(null);
+    navigate('/');  // Redirect to the home page after signing out
   };
 
   return (
-    <nav>
-      {user ? (
-        <ul>
-          <li>Welcome, {user.username}</li>
-          <li><Link to='/'>Dashboard</Link></li>
-          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+    <nav className="navbar">
+      <div className="nav-container">
+        <Link to="/" className="logo">StackUp</Link>
+        <ul className="nav-links">
+          {user ? (
+            <>
+              <li>Welcome, {user.username}</li>
+              <li><Link to="/dashboard">Dashboard</Link></li>
+              <li><Link to="/create">Create Board</Link></li>
+              <li><Link to="/search">Board Search</Link></li>
+              <li>
+                <button className="sign-out-btn" onClick={handleSignOut}>Log Out</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/sign-in"><button className="sign-in-btn">Sign In</button></Link></li>
+              <li><Link to="/sign-up"><button className="sign-up-btn">Sign Up</button></Link></li>
+            </>
+          )}
         </ul>
-      ) : (
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/sign-in'>Sign In</Link></li>
-          <li><Link to='/sign-up'>Sign Up</Link></li>
-        </ul>
-      )}
+      </div>
     </nav>
   );
 };
