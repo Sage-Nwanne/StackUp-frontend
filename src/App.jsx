5a3bbe6d0f592533c4259e000e655b3946bd3893
User Name <user@email.com>
2025-03-19 14:40:49 -0400

    boards rendered on dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
-import { useContext } from 'react';
+// react
+import { useContext, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router';
 
+//components
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import * as boardService from './services/boardService';
 
+//contexts
 import { UserContext } from './contexts/UserContext';
 
+
 const App = () => {
+  const [boards, setBoards] = useState([]);
   const { user } = useContext(UserContext);
-  
+
+  //fetch all boards
+  useEffect(() => {
+   const fetchAllBoards = async () => {
+    try {
+      const boardsData = await boardService.index();
+      
+      setBoards(boardsData);
+    } catch (error) {
+      console.log(error);
+    }
+
+    };
+    if (user) fetchAllBoards();
+  }, [user]) ;
+
+    
+  console.log(boards)
+
   return (
     <>
       <NavBar/>
       <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
+        <Route path='/' element={user ? <Dashboard boards={boards}/> : <Landing />} />
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
       </Routes>
