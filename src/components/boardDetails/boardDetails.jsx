3977adab66432fb51bd02826724165417d8692de
User Name <user@email.com>
2025-03-19 14:48:05 -0400

    added bones of boardDetails

---
+++ src/components/boardDetails/boardDetails.jsx
@@ -0,0 +1,12 @@
+
+
+const BoardDetails = () => {
+  return (
+    <main>
+      <h1>Board Details</h1>
+      <p>This is the board details page.</p>
+    </main>
+  );
+};
+
+export default BoardDetails;
